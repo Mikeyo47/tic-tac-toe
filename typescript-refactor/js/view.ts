af4955da87c1a1589338bd85841738eb76b1830e
Mikeyo47 <kopecky_michal@yahoo.com>
2023-12-01 15:35:07 +0000

    convert store.ts to typescript and start converting view.ts

---
+++ typescript-refactor/js/view.ts
@@ -0,0 +1,164 @@
+export default class View {
+  $ = {};
+  $$ = {};
+
+  constructor() {
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuItems = this.#qs('[data-id="menu-items"]');
+    this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.modal = this.#qs('[data-id="modal"]');
+    this.$.modalText = this.#qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+    this.$.p1wins = this.#qs('[data-id="p1-wins"]');
+    this.$.p2wins = this.#qs('[data-id="p2-wins"]');
+    this.$.ties = this.#qs('[data-id="ties"]');
+    this.$.grid = this.#qs('[data-id="grid"]');
+
+    this.$$.squares = this.#qsAll('[data-id="square"]');
+
+    // UI-only event listeners
+    this.$.menuBtn.addEventListener("click", () => {
+      this.#toggleMenu();
+    });
+  }
+
+  render(game, stats) {
+    const { playerWithStats, ties } = stats;
+    const {
+      moves,
+      currentPlayer,
+      status: { isComplete, winner },
+    } = game;
+
+    this.#closeAll();
+    this.#clearMoves();
+
+    this.#updateScoreboard(
+      playerWithStats[0].wins,
+      playerWithStats[1].wins,
+      ties
+    );
+    this.#initializeMoves(moves);
+
+    if (isComplete) {
+      this.#openModal(winner ? `${winner.name} wins!` : "Tie!");
+      return;
+    }
+
+    this.#setTurnIndicator(currentPlayer);
+  }
+
+  bindGameResetEvent(handler) {
+    this.$.resetBtn.addEventListener("click", handler);
+    this.$.modalBtn.addEventListener("click", handler);
+  }
+
+  bindNewRoundEvent(handler) {
+    this.$.newRoundBtn.addEventListener("click", handler);
+  }
+
+  bindPlayerMoveEvent(handler) {
+    this.#delegate(this.$.grid, '[data-id="square"]', "click", handler);
+  }
+
+  #updateScoreboard(p1Wins, p2Wins, ties) {
+    this.$.p1wins.innerText = `${p1Wins} Wins`;
+    this.$.p2wins.innerText = `${p2Wins} Wins`;
+    this.$.ties.innerText = `${ties}`;
+  }
+
+  #openModal(message) {
+    this.$.modal.classList.remove("hidden");
+    this.$.modalText.innerText = message;
+  }
+
+  #closeAll() {
+    this.#closeModal();
+    this.#closeMenu();
+  }
+
+  #clearMoves() {
+    this.$$.squares.forEach((square) => {
+      square.replaceChildren();
+    });
+  }
+
+  #initializeMoves(moves) {
+    this.$$.squares.forEach((square) => {
+      const existingMove = moves.find((move) => move.squareId === +square.id);
+
+      if (existingMove) {
+        this.#handlePlayerMove(existingMove.player, square);
+      }
+    });
+  }
+
+  #closeModal() {
+    this.$.modal.classList.add("hidden");
+  }
+
+  #closeMenu() {
+    this.$.menuItems.classList.add("hidden");
+    this.$.menuBtn.classList.remove("border");
+    const icon = this.$.menuBtn.querySelector("i");
+    icon.classList.remove("fa-chevron-up");
+    icon.classList.add("fa-chevron-down");
+  }
+
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+    icon.classList.toggle("fa-chevron-up");
+    icon.classList.toggle("fa-chevron-down");
+  }
+
+  #handlePlayerMove(player, squareEl) {
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", player.iconClass, player.colorClass);
+    squareEl.replaceChildren(icon);
+  }
+
+  #setTurnIndicator(player) {
+    const icon = document.createElement("i");
+    const label = document.createElement("p");
+
+    icon.classList.add("fa-solid", player.iconClass, player.colorClass);
+
+    label.classList.add(player.colorClass);
+    label.innerText = `${player.name}, you're up!`;
+
+    this.$.turn.replaceChildren(icon, label);
+  }
+
+  #qs(selector: string, parent: Element) {
+    const el = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector);
+
+    if (!el) throw Error("Could not find element with selector: " + selector);
+    return el;
+  }
+
+  #qsAll(selector, parent) {
+    const elList = parent
+      ? parent.querySelectorAll(selector)
+      : document.querySelectorAll(selector);
+
+    if (!elList)
+      throw Error("Could not find elements with selector: " + selector);
+    return elList;
+  }
+
+  #delegate(el, selector, eventKey, handler) {
+    el.addEventListener(eventKey, (event) => {
+      if (event.target.matches(selector)) {
+        handler(event.target);
+      }
+    });
+  }
+}
